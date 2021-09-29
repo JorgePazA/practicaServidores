89464ccb255956abb19bb3e07e93dc4c6aa1e18a
JorgePazA <jorgepaz1993@gmail.com>
2021-09-28 19:29:32 -0500

    Servidor Funcionando

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require ('express');
+const path = require ('path');
+const app = express();
+
+app.get('/', (req,res) =>{
+    res.sendFile(path.join(__dirname, '/views/index.html'))
+});
+
+app.get('/babbage', (req,res) =>{
+    res.sendFile(path.join(__dirname, '/views/babbage.html'))
+});
+
+app.get('/berners-lee', (req,res) =>{
+    res.sendFile(path.join(__dirname, '/views/berners-lee.html'))
+});
+
+app.get('/clarke', (req,res) =>{
+    res.sendFile(path.join(__dirname, '/views/clarke.html'))
+});
+
+app.get('/hopper', (req,res) =>{
+    res.sendFile(path.join(__dirname, '/views/hopper.html'))
+});
+
+app.get('/turing', (req,res) =>{
+    res.sendFile(path.join(__dirname, '/views/turing.html'))
+});
+
+app.get("/hamilton", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/hamilton.html"))
+});
+
+app.get('/lovelace', (req,res) =>{
+    res.sendFile(path.join(__dirname, '/views/lovelace.html'))
+});
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.listen(3030, () => 
+    console.log('Servidor corriendo...')
+);
+
+
